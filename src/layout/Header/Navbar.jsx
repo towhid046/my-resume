40d984e40e5492f648638831804f7e3d1d8c1811
towhid046 <tamzidislam788@gmail.com>
2023-12-05 16:54:31 -0600

    [update navbar]

--- src/layout/Header/Navbar.jsx
+++ src/layout/Header/Navbar.jsx
@@ -1,21 +1,27 @@
+import { useState } from "react";
 import "./navBar.css";
 import { IoClose } from "react-icons/io5";
 import { LuMenu } from "react-icons/lu";
 import SmBtnWithBorder from "../../Components/SharedComponents/SmBtnWithBorder";
 
 const NavBar = () => {
+  const [isChecked, setChecked] = useState(false);
+
+  const toggleMenu = () => {
+    setChecked(!isChecked);
+  };
 
   // logo:
-const logo = "T";
+  const logo = "T";
 
-// Menu:
-const manuItems = [
-  { title: "Home", url: "#" },
-  { title: "About", url: "#about" },
-  { title: "Skill", url: "#skill" },
-  { title: "Project", url: "#project" },
-  { title: "Contact", url: "#contact" },
-];
+  // Menu:
+  const menuItems = [
+    { title: "Home", url: "#" },
+    { title: "About", url: "#about" },
+    { title: "Skill", url: "#skill" },
+    { title: "Project", url: "#project" },
+    { title: "Contact", url: "#contact" },
+  ];
 
   return (
     <>
@@ -26,12 +32,16 @@ const manuItems = [
               <a href="#">{logo}</a>
             </div>
             <div className="">
-              <input type="checkbox" id="check" />
+              <input type="checkbox" id="check" checked={isChecked} onChange={toggleMenu} />
               <ul className="menu  flex gap_20 color_ash fw_400 ai_c">
-                {manuItems.map((item) => (
+                {menuItems.map((item) => (
                   <li key={Math.random() * 10000000}>
-                    <a className="py_10" href={item.url}>
-                       {item.title.toUpperCase()}
+                    <a
+                      className="py_10"
+                      href={item.url}
+                      onClick={() => setChecked(false)}
+                    >
+                      {item.title.toUpperCase()}
                     </a>
                   </li>
                 ))}
