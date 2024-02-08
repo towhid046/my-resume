6b38fa5045de900e7bd67c31a471762f1f1ac5b2
towhid046 <towhidmorol46@gmail.com>
2024-02-08 20:10:51 +0600

    update some data

--- src/database/contactData.js
+++ src/database/contactData.js
@@ -3,10 +3,10 @@ I am interested in working with any company that thinks my skill will be helpful
 If you are looking for someone like me, please let me know. Or you can just 'say hi' to me.`;
 
 const contactWays = [
-{ title: "Email", name: "tamzidislam788@gmail.com" },
+{ title: "Email", name: "towhidmorol46@gmail.com" },
 { title: "Phone", name: "++0 1928182891" },
-{ title: "Skype", name: "tawhid046pad@gmail.com" },
-{ title: "Address", name: "Dhaka, Bangladesh" },
+{ title: "Skype", name: "towhidmorol46@gmail.com" },
+{ title: "Address", name: "Gopalganj, Dhaka, Bangladesh" },
 ];
 
 export const contactData = {contactTitle, contactWays}
